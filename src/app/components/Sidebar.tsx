9822980e2818dc501edd1934cce573262ce90301
peppapig450 <peppapig450@pm.me>
2024-08-26 10:16:18 -0700

    get the sidebar looking nice and pretty like the template

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,18 +3,16 @@ import {
   Drawer,
   Link,
   List,
-  ListItem,
   ListItemIcon,
   ListItemText,
   Typography,
   Avatar,
-  Button,
   IconButton,
   Grid,
-  Container,
   Box,
   Divider,
   useTheme,
+  IconProps,
   ListItemButton,
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
@@ -32,10 +30,10 @@ const Sidebar: React.FC = () => {
     <Drawer
       variant="permanent"
       sx={{
-        width: theme.spacing(30),
+        width: theme.spacing(37.5),
         flexShrink: 0,
         "& .MuiDrawer-paper": {
-          width: theme.spacing(30),
+          width: theme.spacing(37.5),
           boxSizing: "border-box",
           bgcolor: theme.palette.primary.main,
           color: theme.palette.primary.contrastText,
@@ -53,39 +51,55 @@ const Sidebar: React.FC = () => {
           <Box
             display="flex"
             alignItems="center"
-            flexDirection="column"
+            justifyContent="space-between"
             py={4}
+            px={2}
             sx={{ bgcolor: theme.palette.primary.dark }}
           >
             <Avatar
               alt="Nick Brady"
               src="/images/avatar.jpg"
-              sx={{ width: 80, height: 80, mb: 2 }}
+              sx={{ width: 60, height: 60 }}
             />
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{
-                fontWeight: "bold",
-                color: theme.palette.primary.contrastText,
-              }}
-            >
-              Nick Brady
-            </Typography>
-            <Typography
-              variant="body2"
-              color={theme.palette.primary.contrastText}
-            >
-              Software Developer
-            </Typography>
+            <Box sx={{ textAlign: "right" }}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  fontWeight: "bold",
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Nick Brady
+              </Typography>
+              <Typography
+                variant="body2"
+                color={theme.palette.primary.contrastText}
+              >
+                Software Developer
+              </Typography>
+            </Box>
           </Box>
 
           <Divider sx={{ bgcolor: theme.palette.divider }} />
         </Box>
 
         {/* Navigation Links */}
-        <List>
-          <ListItemButton LinkComponent={NextLink} href="#top">
+        <List sx={{ flexGrow: 1, width: "100%" }}>
+          <ListItemButton
+            LinkComponent={NextLink}
+            href="#top"
+            sx={{ justifyContent: "space-between", pl: 3, pr: 2 }}
+          >
+            <ListItemIcon
+              sx={{
+                color: theme.palette.secondary.light,
+                minWidth: "auto",
+                mr: 2,
+              }}
+            >
+              <HomeIcon />
+            </ListItemIcon>
             <ListItemText
               primary="Intro"
               primaryTypographyProps={{
@@ -93,11 +107,117 @@ const Sidebar: React.FC = () => {
                 color: theme.palette.primary.contrastText,
               }}
             />
-            <ListItemIcon sx={{ color: theme.palette.secondary.light }}>
-              <HomeIcon />
+          </ListItemButton>
+          <ListItemButton
+            LinkComponent={NextLink}
+            href="#portfolio"
+            sx={{ justifyContent: "space-between", pl: 3, pr: 2 }}
+          >
+            <ListItemIcon
+              sx={{
+                color: theme.palette.secondary.light,
+                minWidth: "auto",
+                mr: 2,
+              }}
+            >
+              <WorkIcon />
             </ListItemIcon>
+            <ListItemText
+              primary="Portfolio"
+              primaryTypographyProps={{
+                textAlign: "right",
+                color: theme.palette.primary.contrastText,
+              }}
+            />
+          </ListItemButton>
+
+          <ListItemButton
+            LinkComponent={NextLink}
+            href="#about"
+            sx={{ justifyContent: "space-between", pl: 3, pr: 2 }}
+          >
+            <ListItemIcon
+              sx={{
+                color: theme.palette.secondary.light,
+                minWidth: "auto",
+                mr: 2,
+              }}
+            >
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary="About Me"
+              primaryTypographyProps={{
+                textAlign: "right",
+                color: theme.palette.primary.contrastText,
+              }}
+            />
+          </ListItemButton>
+
+          <ListItemButton
+            LinkComponent={NextLink}
+            href="#contact"
+            sx={{ justifyContent: "space-between", pl: 3, pr: 2 }}
+          >
+            <ListItemIcon
+              sx={{
+                color: theme.palette.secondary.light,
+                minWidth: "auto",
+                mr: 2,
+              }}
+            >
+              <EmailIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary="Contact"
+              primaryTypographyProps={{
+                textAlign: "right",
+                color: theme.palette.primary.contrastText,
+              }}
+            />
           </ListItemButton>
         </List>
+      </Box>
+
+      {/* Bottom Section: Social Links */}
+      <Box>
+        <Divider sx={{ bgcolor: theme.palette.divider }} />
+        <Grid container justifyContent="center" spacing={2} p={2}>
+          <Grid item>
+            <Link
+              href="https://github.com/peppapig450"
+              target="_blank"
+              rel="noopener noreferrer"
+              component={NextLink}
+              passHref
+            >
+              <IconButton
+                color="inherit"
+                size="large"
+                sx={{ color: theme.palette.secondary.main }}
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link
+              href="https://www.linkedin.com/in/nick-brady-5715752b3"
+              target="_blank"
+              rel="noopener noreferrer"
+              component={NextLink}
+              passHref
+            >
+              <IconButton
+                color="inherit"
+                size="large"
+                sx={{ color: theme.palette.secondary.main }}
+              >
+                <LinkedInIcon />
+              </IconButton>
+            </Link>
+          </Grid>
+        </Grid>
       </Box>
     </Drawer>
   );
