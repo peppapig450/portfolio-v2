51e76337dc5ea4e0ca3c939e5459cefcbf5dd3e9
peppapig450 <nbgenius1@gmail.com>
2024-08-27 14:57:03 -0700

    rewrite the home page to have padding taking into account the sidebar for the other components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-'use client';
+"use client";
 import Image from "next/image";
+import { Container, Box, useTheme } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 
 export default function Home() {
+  const theme = useTheme();
   return (
-    <Sidebar />
+    <>
+      <Sidebar />
+      <Container
+        maxWidth={false}
+        disableGutters
+        sx={{
+          pl: { xs: "0", sm: theme.spacing(37.5) },
+          transition: theme.transitions.create("padding-left"),
+        }}
+      >
+        <Box component="main">{/* Add all other sections here */}</Box>
+      </Container>
+    </>
   );
 }
