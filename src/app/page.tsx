aad3454023dc21254d21ea019ea702ad5d11205c
peppapig450 <peppapig450@pm.me>
2024-08-27 18:46:41 -0700

    keep trying to animate the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
 import Layout from "./components/Layout";
+import IntroPageContent from "./components/Sections/IntroSection";
 
 export default function Home() {
   return (
     <Layout>
-      <></>
+      <IntroPageContent />
     </Layout>
   );
 }
