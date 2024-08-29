0b5bb0f05fdd75afffe0d7f6f0c9fdfff6e88bc8
peppapig450 <nbgenius1@gmail.com>
2024-08-29 10:38:07 -0700

    write the paragraph  portfion of the intro section

--- src/app/components/Sections/IntroSection.tsx
+++ src/app/components/Sections/IntroSection.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { NextPage } from "next";
-import Link from "next/link";
-import { Box, Typography, useTheme } from "@mui/material";
+import NextLink from "next/link";
+import { Box, Typography, Link, useTheme } from "@mui/material";
 import { motion } from "framer-motion";
 import { noiseAnim, noiseAnim2 } from "@/styles/keyframes";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
@@ -65,6 +64,65 @@ const IntroPageContent: React.FC<{}> = ({}) => {
           </Typography>
         </motion.div>
       </Box>
+      <motion.div {...getTransitions(0.2)}>
+        <Box sx={{ px: theme.spacing(3) }}>
+          <Typography
+            variant="body1"
+            component="p"
+            sx={{ mt: theme.spacing(2) }}
+          >
+            Your friendly neighborhood versatile full-stack developer, AI
+            engineer, and Python enthusiast. I spend my days and free time
+            painting computer screens with{" "}
+            <Link
+              href="/projects"
+              component={NextLink}
+              underline="none"
+              passHref
+              aria-label="Go to Projects page"
+              sx={{ fontWeight: "bold", color: theme.palette.primary.main }}
+            >
+              PROJECTS
+            </Link>{" "}
+            and lines of code, turning binary into beautiful, performant
+            applications.
+          </Typography>
+          {/* TODO: maybe add my photography stuff */}
+          <Typography
+            variant="body1"
+            component="p"
+            sx={{ mt: theme.spacing(1) }}
+          >
+            I pride myself in versatility, able to build a wide variety of
+            applications using a wide variety of tools. Whether it be
+            minimilism, raw usability, Material design, I got it covered
+            building performant frontends and backends. I also enjoy building
+            GUI and command line applications using Java and Python. When
+            I&apos;m not coding, you can catch me reading{" "}
+            <Link
+              href="/"
+              component={NextLink}
+              underline="none"
+              passHref
+              aria-label="Check out articles I read."
+              sx={{ fontWeight: "bold", color: theme.palette.primary.main }}
+            >
+              ARTICLES
+            </Link>
+            . Anywho you can{" "}
+            <Link
+              href="/contact"
+              component={NextLink}
+              underline="none"
+              passHref
+              aria-label="Go to Contact Page"
+              sx={{ fontWeight: "bold", color: theme.palette.primary.main }}
+            >
+              CONTACT ME
+            </Link>
+          </Typography>
+        </Box>
+      </motion.div>
     </Box>
   );
 };
