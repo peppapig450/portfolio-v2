1536bd498b2b7557662736f58d0961567c67eb31
peppapig450 <peppapig450@pm.me>
2024-08-30 14:17:07 -0700

    use our custom fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,18 @@
 "use client";
 import { createTheme } from "@mui/material";
+import { Labrada, Cardo } from "next/font/google";
+
+const labrada = Labrada({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
+
+const cardo = Cardo({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
 
 const mainTheme = createTheme({
   palette: {
@@ -9,6 +22,15 @@ const mainTheme = createTheme({
     secondary: {
       main: "#f50057",
     },
+  },
+  typography: {
+    fontFamily: cardo.style.fontFamily,
+    h1: { fontFamily: labrada.style.fontFamily },
+    h2: { fontFamily: labrada.style.fontFamily },
+    h3: { fontFamily: labrada.style.fontFamily },
+    h4: { fontFamily: labrada.style.fontFamily },
+    h5: { fontFamily: labrada.style.fontFamily },
+    h6: { fontFamily: labrada.style.fontFamily },
   },
 });
 
