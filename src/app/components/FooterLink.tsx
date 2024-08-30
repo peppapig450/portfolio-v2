4163fb46b2566b1d9385ee853c672d8fe75d4472
peppapig450 <nbgenius1@gmail.com>
2024-08-30 14:23:30 -0700

    underline the text when hovered, make the arrow bigger and move more in the animation

--- src/app/components/FooterLink.tsx
+++ src/app/components/FooterLink.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import NextLink from "next/link";
-import { Box, Typography, Link, IconButton, useTheme } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Link,
+  TypographyProps,
+  IconButton,
+  useTheme,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { arrowBounce } from "@/styles/keyframes";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { arrowBounce, underlineAnimation } from "@/styles/keyframes";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -16,32 +23,54 @@ interface IFooterLink {
   goto?: string;
 }
 
+const AnimatedTypography = styled(Typography)<TypographyProps>(({ theme }) => ({
+  position: "relative",
+  display: "inline-block",
+  "&::after": {
+    content: '""',
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    width: "100%",
+    height: "2px",
+    backgroundColor: theme.palette.primary.main,
+    transform: "scaleX(0)",
+    transformOrigin: "bottom right",
+    transition: "transform 0.3s ease",
+  },
+  "&:hover::after": {
+    transform: "scaleX(1)",
+    transformOrigin: "bottom left",
+  },
+}));
+
 const FooterLink: React.FC<IFooterLink> = ({ children, goto = "/" }) => {
   const theme = useTheme();
 
   return (
     <Box component="footer" sx={{ mt: 4 }}>
-      <Link href="{goto}" underline="none" component={NextLink} passHref>
-        <Typography
-          sx={{
-            fontSize: theme.typography.body2.fontSize,
-            fontWeight: theme.typography.fontWeightMedium,
-            display: "inline-flex",
-            alignItems: "center",
-            mb: theme.spacing(2),
-            "&:hover .MuiSvgIcon-root": {
-              ml: theme.spacing(2),
-            },
-          }}
-        >
-          {children}
-          <ArrowForwardIcon
+      <Link href={goto} underline="none" component={NextLink} passHref>
+        <Box sx={{ display: "inline-flex", alignItems: "center" }}>
+          <AnimatedTypography
             sx={{
+              fontSize: theme.typography.body1.fontSize,
+              fontWeight: theme.typography.fontWeightMedium,
+              color: theme.palette.grey[800],
+              display: "inline-flex",
+              alignItems: "center",
+            }}
+          >
+            {children}
+          </AnimatedTypography>
+          <ArrowRightAltIcon
+            sx={{
+              fontSize: "60px",
               ml: theme.spacing(1),
-              animation: `${arrowBounce} 0.5 infinite alternate`,
+              color: theme.palette.secondary.main,
+              animation: `${arrowBounce} 0.5s infinite alternate`,
             }}
           />
-        </Typography>
+        </Box>
       </Link>
     </Box>
   );
