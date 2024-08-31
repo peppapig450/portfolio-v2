e78c604d321c240bef0fe2ccbeb94a25bd769407
peppapig450 <peppapig450@pm.me>
2024-08-31 09:13:29 -0700

    refactor the intro section to use our components for cleaner code

--- src/app/components/Sections/IntroSection.tsx
+++ src/app/components/Sections/IntroSection.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import NextLink from "next/link";
-import { Box, Typography, Link, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { motion } from "framer-motion";
 import { getTransitions } from "../utils/transitions";
 import AnimatedTitle from "../LandingPage/AnimatedTitle";
 import FooterLink from "../FooterLink";
+import IntroductionText from "../LandingPage/IntroductionText";
 
-// TODO: Refactor this into resusable components and stylized components
 const IntroPageContent: React.FC<{}> = ({}) => {
   const theme = useTheme();
 
@@ -27,94 +26,9 @@ const IntroPageContent: React.FC<{}> = ({}) => {
       </motion.div>
 
       <Box>
-        <motion.div {...getTransitions(0.2)}>
-          <Typography
-            variant="body1"
-            component="p"
-            align="justify"
-            sx={{ lineHeight: "2.3" }}
-          >
-            Welcome! I&apos;m a versatile full-stack developer and AI engineer,
-            blending creativity with technical expertise. From sleek frontends
-            to robust backends, I craft high-performance applications across the
-            full tech stack. My diverse toolkit spans Java, Python, React, and
-            more, allowing me to tackle any{" "}
-            <Link
-              href="/projects"
-              component={NextLink}
-              underline="none"
-              passHref
-              aria-label="Go to Projects page"
-              sx={{
-                fontWeight: "bold",
-                border: "medium",
-                textTransform: "uppercase",
-                color: theme.palette.primary.main,
-              }}
-            >
-              project
-            </Link>{" "}
-            with innovative solutions.
-          </Typography>
-        </motion.div>
-        <motion.div {...getTransitions(0.3)}>
-          <Typography
-            variant="body1"
-            component="p"
-            sx={{ mt: theme.spacing(1), lineHeight: "2.3" }}
-          >
-            Off-screen, I&apos;m either exploring tech insights through{" "}
-            <Link
-              href="/"
-              component={NextLink}
-              underline="none"
-              passHref
-              aria-label="Check out articles I read."
-              sx={{
-                fontWeight: "bold",
-                border: "medium",
-                textTransform: "uppercase",
-                color: theme.palette.primary.main,
-              }}
-            >
-              articles
-            </Link>{" "}
-            or finding inspiration through{" "}
-            <Link
-              href="/photography"
-              component={NextLink}
-              underline="none"
-              passHref
-              aria-label="Check out my photography."
-              sx={{
-                fontWeight: "bold",
-                border: "medium",
-                textTransform: "uppercase",
-                color: theme.palette.primary.main,
-              }}
-            >
-              photography
-            </Link>
-            . Ready to bring a unique perspective to your next project?{" "}
-            <Link
-              href="/contact"
-              component={NextLink}
-              underline="none"
-              passHref
-              aria-label="Go to Contact Page"
-              sx={{
-                fontWeight: "bold",
-                border: "medium",
-                textTransform: "uppercase",
-                color: theme.palette.primary.main,
-              }}
-            >
-              contact me
-            </Link>{" "}
-            and let&apos;s create something extraordinary.
-          </Typography>
-        </motion.div>
+        <IntroductionText />
       </Box>
+
       <motion.div {...getTransitions(0.7)}>
         <FooterLink goto="/about">See More About Me</FooterLink>
       </motion.div>
