56c3a1b46ab3a03b30c62950bf8cae87b8bb50c1
peppapig450 <peppapig450@pm.me>
2024-08-31 09:12:37 -0700

    write wrapper around material ui link with nextlink

---
+++ src/app/components/LandingPage/CustomLink.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import NextLink from "next/link";
+import { Link, LinkProps } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+interface CustomLinkProps extends Omit<LinkProps, "component"> {
+  href: string;
+  children: React.ReactNode;
+}
+
+const CustomLink: React.FC<CustomLinkProps> = ({
+  href,
+  children,
+  ...props
+}) => {
+  return <Link component={NextLink} href={href} passHref {...props}></Link>;
+};
+
+export default CustomLink;
