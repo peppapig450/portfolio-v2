0af2cc40c84ee1123a1959afe723cc3771f0bc9b
peppapig450 <nbgenius1@gmail.com>
2024-08-31 18:08:25 -0700

    update the metadata to start using dynamic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,23 @@ import { Analytics } from "@vercel/analytics/react";
 import mainTheme from "@/styles/theme";
 
 export const metadata: Metadata = {
-  title: "Nick Brady",
-  description: "Software Developer Portfolio",
+  title: {
+    template: "%s | Nick Brady",
+    default: "Portfolio",
+  },
+  description:
+    "Hi, I&#x27;m Nick Brady, Full-stack developer, software engineer, and problem-solver proficient in React, Typescript, Java, OpenAI, Python, and more.",
+  generator: "Next.js",
+  applicationName: "Nick Brady&#x27;s portfolio",
+  referrer: "origin-when-cross-origin",
+  authors: [{ name: "Nick Brady", url: "https://github.com/peppapig450" }],
+  creator: "Nick Brady",
+  publisher: "Nick Brady",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
