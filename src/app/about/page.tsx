3d736948559e2e470e5049d594943993db18bcb7
peppapig450 <nbgenius1@gmail.com>
2024-09-02 12:29:36 -0700

    add the layout and metadata to the new about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,17 @@
+import type { Metadata } from "next";
+import Layout from "../components/Layout";
+
+export const metadata: Metadata = {
+  title: "About Me | Nick Brady",
+  openGraph: {
+    title: "About Me | Nick Brady",
+  },
+};
+
 export default function About() {
-  return <></>;
+  return (
+    <Layout>
+      <></>
+    </Layout>
+  );
 }
