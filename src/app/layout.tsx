f84334de107210ea507224fbffd119b6d6e99b1c
peppapig450 <nbgenius1@gmail.com>
2024-09-01 17:58:17 -0700

    change default title to reflect the portfolio website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import mainTheme from "@/styles/theme";
 export const metadata: Metadata = {
   title: {
     template: "%s | Nick Brady",
-    default: "Home | Nick Brady",
+    default: "Nick Brady&#x27;s Portfolio",
   },
   description:
     "Hi, I&#x27;m Nick Brady, Full-stack developer, software engineer, and problem-solver proficient in React, Typescript, Java, AI applications, Python, and more.",
