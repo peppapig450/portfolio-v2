623b15959622fa6cf2b9abf2af291b8b44debb8a
peppapig450 <peppapig450@pm.me>
2024-09-02 14:45:45 -0700

    create an exported SectionWrapper for easy use of the 'section' component with our bootstrap style spacing

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,11 +1,45 @@
 "use client";
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { Box, useTheme, Container } from "@mui/material";
 import NavBar from "./Navbar";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
+interface PageWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+  // Additional props can be added here if needed
+}
+
+export const SectionWrapper: React.FC<PropsWithChildren<PageWrapperProps>> = ({
+  children,
+  ...rest
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Box component="section" sx={{ width: "100%" }} {...rest}>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          [theme.breakpoints.up("sm")]: {
+            maxWidth: "720px",
+          },
+          [theme.breakpoints.up("md")]: {
+            maxWidth: "960px",
+          },
+          [theme.breakpoints.up("lg")]: {
+            maxWidth: "1140px",
+          },
+        }}
+      >
+        {children}
+      </Container>
+    </Box>
+  );
+};
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const theme = useTheme();
@@ -15,27 +49,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <Box component="header" sx={{ py: theme.spacing(5) }}>
         <NavBar />
       </Box>
-      <Box component="section" sx={{ width: "100%" }}>
-        <Container
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            [theme.breakpoints.up("sm")]: {
-              maxWidth: "720px",
-            },
-            [theme.breakpoints.up("md")]: {
-              maxWidth: "960px",
-            },
-            [theme.breakpoints.up("lg")]: {
-              maxWidth: "1140px",
-            },
-          }}
-        >
-          {children}
-        </Container>
-      </Box>
+      <SectionWrapper>{children}</SectionWrapper>
     </>
   );
 };
