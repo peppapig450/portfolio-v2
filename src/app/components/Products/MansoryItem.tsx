e3ea8acf936267c84e06f4ac762cb5fde39f9b9e
peppapig450 <nbgenius1@gmail.com>
2024-09-03 10:30:52 -0700

    add imports to the mansory item

---
+++ src/app/components/Products/MansoryItem.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Chip,
+  Typography,
+  Box,
+  Dialog,
+  DialogContent,
+  styled,
+} from "@mui/material";
