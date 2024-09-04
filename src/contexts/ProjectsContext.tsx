d449965319367ccf7c7baf3a1f867e58f41fbdbe
peppapig450 <nbgenius1@gmail.com>
2024-09-03 23:46:22 -0700

    write skeleton for first project, the filter by type, the provider, and the hook for using the context

--- src/contexts/ProjectsContext.tsx
+++ src/contexts/ProjectsContext.tsx
@@ -27,3 +27,53 @@ interface ProjectsContextType {
 const ProjectsContext = createContext<ProjectsContextType | undefined>(
   undefined
 );
+
+export const ProvidersProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const projects_data: Project[] = [
+    {
+      title: "ProfPick",
+      description: "Using AI to make sure you get the best professor.",
+      about: "",
+      link: "https://rate-my-professor-lac.vercel.app/",
+      imageUrl: "",
+      github: "https://github.com/HeadstarterVenomBytes/Rate-My-Professor",
+      type: [ProjectType.Project, ProjectType.OpenSource, ProjectType.Others],
+      technologies: [
+        "Python",
+        "React / Next.js",
+        "TypeScript",
+        "OpenAI",
+        "Pinecone",
+        "LangChain",
+        "RAG",
+        "Cheerio",
+      ],
+    },
+  ];
+  const getProjectsByType = (type: ProjectType): Project[] => {
+    return projects_data.filter((project) => project.type.includes(type));
+  };
+
+  const value: ProjectsContextType = {
+    projects: projects_data,
+    getProjectsByType,
+  };
+
+  return (
+    <ProjectsContext.Provider value={value}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export const useProjectsContext = (): ProjectsContextType => {
+  const context = useContext(ProjectsContext);
+  if (context === undefined) {
+    throw new Error(
+      "useProjectsContext must be used within a Projects Provider"
+    );
+  }
+  return context;
+};
