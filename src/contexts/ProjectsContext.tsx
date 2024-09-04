72f5a4fab3529824402addab8cb33958c7a5445e
peppapig450 <nbgenius1@gmail.com>
2024-09-04 16:21:24 -0700

    seo optimize about and description, and add image name

--- src/contexts/ProjectsContext.tsx
+++ src/contexts/ProjectsContext.tsx
@@ -14,7 +14,7 @@ interface Project {
   description: string;
   about: string;
   link: string;
-  imageUrl: string;
+  imageName: string;
   github?: string;
   type: ProjectType[];
   technologies: string[];
@@ -38,11 +38,11 @@ export const ProvidersProvider: React.FC<{ children: ReactNode }> = ({
     {
       title: "ProfPick",
       description:
-        "Get the perfect professor for your needs with ProfPick's AI-powered recommendations.",
+        "ProfPick uses AI to deliver personalized professor recommendations based on Rate My Professor data and user-submitted reviews.",
       about:
-        "ProfPick uses data scraped from ratemyprofesor.com to enable AI powered recommendations based on what the user expects. It also allows users to upload their own links to be scraped and added to the database.",
+        "ProfPick is a revolutionary tool designed to streamline the class selection process. Powered by advanced AI algorithms, we analyze vast amounts of data from Rate My Professor to provide accurate and relevant professor recommendations tailored to your specific needs. With ProfPick, you can effortlessly discover the best professors for your courses and make informed decisions about your academic journey. Easily upload your own professor reviews to contribute to the community and help other students.",
       link: "https://rate-my-professor-lac.vercel.app/",
-      imageUrl: "",
+      imageName: "profpick_demo.avif",
       github: "https://github.com/HeadstarterVenomBytes/Rate-My-Professor",
       type: [ProjectType.Project, ProjectType.OpenSource, ProjectType.Others],
       technologies: [
