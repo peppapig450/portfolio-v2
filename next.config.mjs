e18ff354361380c71b14f26448d5e6d43f360c6f
peppapig450 <peppapig450@pm.me>
2024-09-04 16:20:25 -0700

    add custom image loader for the `imgix` cdn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "personal-portfolio-products.imgix.net",
+            },
+        ],
+        loader: "custom",
+        loaderFile: "loader.ts",
+    }
+};
 
 export default nextConfig;
