00f88008b1767401ef7b5549062c0d7121022929
peppapig450 <peppapig450@pm.me>
2024-09-04 17:22:58 -0700

    finish writing the side bar modal popup that displays project and use the project context's interface for the data typing

--- src/app/components/Products/SideBarModal.tsx
+++ src/app/components/Products/SideBarModal.tsx
@@ -2,14 +2,13 @@
 import React, { useCallback, useEffect } from "react";
 import {
   Drawer,
-  DrawerProps,
   IconButton,
   Typography,
   Box,
   Chip,
   Button,
-  Link,
   styled,
+  useTheme,
 } from "@mui/material";
 import Image from "next/image";
 import CloseIcon from "@mui/icons-material/Close";
@@ -17,39 +16,27 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LanguageIcon from "@mui/icons-material/Language";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import CustomLink from "../CustomLink";
+import { Project } from "@/contexts/ProjectsContext";
 
 interface ISideBarModal {
   show: boolean;
   closeShow: () => void;
   size?: "sm" | "lg" | "md";
   overlayColor?: string;
-  data?: {
-    title: string;
-    description?: string;
-    technologies?: string[];
-    github?: string;
-    imageUrl?: string;
-    about?: string;
-    link?: string;
-  };
+  data?: Project;
 }
-
-const defaultProps: ISideBarModal = {
-  show: false,
-  closeShow: () => {},
-  size: "md",
-  overlayColor: "rgba(0, 0, 0, 0.8)",
-};
 
 const ImageContainer = styled(Box)({
   position: "relative",
+  width: "100%",
+  height: "300px",
   overflow: "hidden",
   borderRadius: 11,
 });
 
 const TechnologyChip = styled(Chip)(({ theme }) => ({
   margin: theme.spacing(0.5),
-  backgroundColor: theme.palette.background.default,
+  backgroundColor: theme.palette.background.paper,
 }));
 
 const OpenProjectButton = styled(Button)(({ theme }) => ({
@@ -60,12 +47,16 @@ const OpenProjectButton = styled(Button)(({ theme }) => ({
   padding: theme.spacing(2),
 }));
 
+// TODO: add animations for fading in from the left
 const SideBarModel: React.FC<ISideBarModal> = ({
-  show,
-  closeShow,
+  show = false,
+  closeShow = () => {},
   size = "md",
+  overlayColor = "rgba(0, 0, 0, 0.8)",
   data,
 }) => {
+  const theme = useTheme();
+
   const handleKeyPress = useCallback(
     (e: KeyboardEvent) => {
       if (e.key === "Escape") {
@@ -91,6 +82,8 @@ const SideBarModel: React.FC<ISideBarModal> = ({
       anchor="right"
       open={show}
       onClose={closeShow}
+      aria-labelledby="product-modal-title"
+      aria-describedby="product-modal-description"
       PaperProps={{
         style: {
           width: drawerWidth,
@@ -130,15 +123,128 @@ const SideBarModel: React.FC<ISideBarModal> = ({
           </CustomLink>
         </Box>
 
-        <Box sx={{ mb: 4 }}>
-          <Typography variant="h4" fontWeight="bold" gutterBottom>
-            {data.title}
-          </Typography>
-          <Typography variant="body1" component="p" color="textSecondary">
-            {data.description}
-          </Typography>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          gutterBottom
+          id="product-modal-title"
+          sx={{
+            mt: theme.spacing(3),
+            mb: theme.spacing(1),
+          }}
+        >
+          {data?.title}
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          color="textSecondary"
+          sx={{
+            mb: theme.spacing(3),
+          }}
+        >
+          {data?.about}
+        </Typography>
+        <ImageContainer>
+          <Image src={data?.imageUrl} alt={data?.imageAlt} fill quality={80} />
+        </ImageContainer>
+
+        <Typography
+          variant="h6"
+          component="h4"
+          sx={{ mb: theme.spacing(1.6), mt: theme.spacing(4) }}
+        >
+          About
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          color="textSecondary"
+          sx={{ mb: theme.spacing(2) }}
+        >
+          {data?.about}
+        </Typography>
+
+        <Typography
+          variant="h6"
+          component="h4"
+          sx={{ mb: theme.spacing(1.6), mt: theme.spacing(4) }}
+        >
+          Technologies
+        </Typography>
+        <Box display="flex" flexWrap="wrap" sx={{ gap: 0.5 }}>
+          {data?.technologies?.map((technology, index) => (
+            <TechnologyChip key={index} label={technology} size="small" />
+          ))}
         </Box>
+
+        <Typography
+          variant="h6"
+          component="h4"
+          sx={{ mb: theme.spacing(1.6), mt: theme.spacing(4) }}
+        >
+          <LanguageIcon sx={{ mr: 1, verticalAlign: "bottom" }} />
+          Website
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          sx={{ mb: theme.spacing(2), fontWeight: 600 }}
+        >
+          <CustomLink
+            href={data?.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            underline="hover"
+          >
+            {data?.link}
+          </CustomLink>
+        </Typography>
+
+        {data.github && (
+          <>
+            <Typography
+              variant="h6"
+              component="h4"
+              sx={{ mb: theme.spacing(1.6), mt: theme.spacing(4) }}
+            >
+              <GitHubIcon sx={{ mr: 1, verticalAlign: "bottom" }} />
+              Github
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              sx={{ mb: theme.spacing(2), fontWeight: 600 }}
+            >
+              <CustomLink
+                href={data.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                underline="hover"
+              >
+                {data.github}
+              </CustomLink>
+            </Typography>
+          </>
+        )}
+
+        <CustomLink
+          href={data.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          underline="hover"
+        >
+          <OpenProjectButton
+            variant="contained"
+            color="primary"
+            endIcon={<OpenInNewIcon />}
+          >
+            Open Project
+          </OpenProjectButton>
+        </CustomLink>
       </Box>
     </Drawer>
   );
 };
+
+export default SideBarModel;
