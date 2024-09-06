b4d6db80e386f62a59b382162b00274bc0be9bbd
peppapig450 <nbgenius1@gmail.com>
2024-09-06 16:12:34 -0700

    support avif, webp, and fix the loader url

--- loader.ts
+++ loader.ts
@@ -9,13 +9,11 @@ export default function imgixLoader({
   width,
   quality,
 }: ImgixLoaderParams): string {
-  const url = new URL(`https://personal-portfolio-products.imgix.net${src}`);
+  const url = new URL(`https://personal-portfolio-products.imgix.net/${src}`);
   const params = url.searchParams;
 
   params.set("auto", params.getAll("auto").join(",") || "format");
   params.set("fit", params.get("fit") || "max");
   params.set("w", params.get("w") || width.toString());
-  params.set("q", (quality ?? 75).toString());
-
   return url.href;
 }
